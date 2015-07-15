5e9bd752edaaf261b04a74cd8f39a4114f6ffc2c
TakeAsh <take@TakeAsh.net>
2015-07-16 07:39:00 +0900

    add DragEventHelper
    - CreateDragOverEventHandler(IEnumerable<string> supportedFormats,
    DragDropEffects effectWhenSupported = DragDropEffects.Copy, bool
    handledWhenNotSupported = true)

---
+++ SmtpClient/DragEventHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace SmtpClient {
+    
+    public static class DragEventHelper {
+
+        public static DragEventHandler CreateDragOverEventHandler(
+            IEnumerable<string> supportedFormats,
+            DragDropEffects effectWhenSupported = DragDropEffects.Copy,
+            bool handledWhenNotSupported = true
+        ) {
+            return (sender, e) => {
+                foreach (var format in supportedFormats) {
+                    if (e.Data.GetDataPresent(format)) {
+                        e.Effects = effectWhenSupported;
+                        e.Handled = true;
+                        return;
+                    }
+                }
+                e.Effects = DragDropEffects.None;
+                e.Handled = handledWhenNotSupported;
+            };
+        }
+    }
+}
