3f34ff2a5312549b8670a7f05862975f10db6aab
TakeAsh <take@TakeAsh.net>
2015-07-15 09:23:14 +0900

    use TakeAshUtility

--- SmtpClient/MainWindow.xaml.cs
+++ SmtpClient/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
-using TakeAsh;  // string.TryParse<T>(), Crypt, FileFilterItem
+using TakeAshUtility;
 
 namespace SmtpClient {
 
